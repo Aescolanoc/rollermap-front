fd8f5c8483d393fd68f5a5046b933625d212a024
Mel Escolano <amalia.escolano@gmail.com>
2022-03-30 22:02:05 +0200

    Updated API test

--- src/services/__tests__/api.spec.js
+++ src/services/__tests__/api.spec.js
@@ -1,26 +1,47 @@
 import axios from "axios";
-import { describe, it, expect, vi, beforeEach } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import * as api from "../api";
 
-// vi.mock("axios", () => ({
-//   ...vi.requireActual("axios"),
-//   post: vi.fn().mockResolvedValue({ data: "Mocked response" }),
-// }));
-
-vi.mock("axios", async () => ({
-  ...(await vi.importActual, "axios"),
-  post: vi.fn().mockResolvedValue({ data: "Mocked response" }),
+vi.mock("axios", () => ({
+  default: {
+    create: vi.fn().mockReturnValue({
+      interceptors: {
+        response: {
+          use: vi.fn().mockImplementation((callback1, callback2) => callback1()),
+        },
+      },
+      post: vi.fn(),
+      get: vi.fn(),
+    }),
+  },
 }));
 
 describe("Testing Api service", () => {
   describe("When function is called", () => {
-    beforeEach(() => {
-      axios.get.mockResolvedValue({ data: "Mocked response" });
-      axios.patch.mockResolvedValue({ data: "Mocked response" });
-      axios.post.mockResolvedValue({ data: "Mocked response" });
-    });
     it("login", () => {
-      expect(api.login({ email: "pepe@pepe.es", password: "1234" })).toEqual({ data: 3 });
+      api.login({ email: "pepe@pepe.com", password: "1234" });
+      expect(axios.create().post).toHaveBeenCalled();
+    });
+  });
+
+  describe("When function is called", () => {
+    it("register", () => {
+      api.register({ email: "pepe@pepe.com", password: "1234" });
+      expect(axios.create().post).toHaveBeenCalled();
+    });
+  });
+
+  describe("When function is called", () => {
+    it("register", () => {
+      api.getUserDetails("1");
+      expect(axios.create().get).toHaveBeenCalled();
+    });
+  });
+
+  describe("When function is called", () => {
+    it("getAllRollerPlaces", () => {
+      api.getAllRollerPlaces();
+      expect(axios.create().get).toHaveBeenCalled();
     });
   });
 });
